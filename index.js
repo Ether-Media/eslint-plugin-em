babbd8ea4c9eb394231f92a567ebd8d96796eb33
Occam <occamguridi@proton.me>
2023-09-03 16:13:01 -0300

    add index.js and rules

---
+++ index.js
@@ -0,0 +1,21 @@
+const allRules = {
+  'sort-style-alphabetically': sort_style_alphabetically,
+};
+
+const rulesConfig = {
+  plugins: [em],
+  parserOptions: {
+    ecmaFeatures: {jsx: true},
+  },
+  rules: {
+    'sort-style-alphabetically': 2,
+  },
+};
+
+module.exports = {
+  rules: allRules,
+  configs: {
+    recommended: rulesConfig,
+    all: rulesConfig,
+  },
+};
