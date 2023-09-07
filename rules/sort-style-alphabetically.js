49a1bc2d2642c3232ff0fe86aa4c934e5d433cf2
Occam <occamguridi@proton.me>
2023-09-06 21:43:25 -0300

    Rule works, god bless

--- rules/sort-style-alphabetically.js
+++ rules/sort-style-alphabetically.js
@@ -18,49 +18,68 @@ module.exports = {
           node.parent.parent.id.name === 'getThemedStyles'
         ) {
           const properties = node.properties;
-          const styles = [];
+          const alphaStyles = [];
+          const otherStyles = [];
 
-          properties.forEach((property, index) => {
+          properties.forEach((property) => {
             if (property.type === 'Property') {
-              styles.push({
-                name: property.key.name,
-                node: property,
-              });
+              // Define a helper function to extract the name based on node type
+              const extractName = (keyNode) => {
+                if (keyNode.type === 'Literal') {
+                  return keyNode.value;
+                } else if (keyNode.type === 'Identifier') {
+                  return keyNode.name;
+                } else {
+                  // Attempt to stringify if it's an object/array/other complex types
+                  try {
+                    return JSON.stringify(keyNode);
+                  } catch (e) {
+                    // If not stringifiable, just return the type as name
+                    return keyNode.type;
+                  }
+                }
+              };
+
+              const name = extractName(property.key);
+
+              if (property.computed) {
+                otherStyles.push({
+                  name: `[${property.key.object.name}.${property.key.property.name}]`,
+                  node: property,
+                });
+              } else if (property.key.type === 'Identifier') {
+                alphaStyles.push({
+                  name,
+                  node: property,
+                });
+              }
             }
           });
 
-          const sortedStyles = styles
-            .slice()
-            .sort((a, b) => a.name.localeCompare(b.name));
+          otherStyles.sort((a, b) => a.name.localeCompare(b.name));
+          alphaStyles.sort((a, b) => a.name.localeCompare(b.name));
 
-          for (let i = 0; i < styles.length; i++) {
-            if (styles[i] !== sortedStyles[i]) {
+          const sortedStyles = alphaStyles.concat(otherStyles);
+
+          properties.map((property, i) => {
+            if (properties[i] !== sortedStyles[i].node) {
               context.report({
-                node: styles[i].node,
+                node: property,
                 message: 'Styles should be in alphabetical order.',
                 fix: (fixer) => {
-                  const startIndex = styles[i].node.range[0];
-                  const endIndex = styles[i].node.range[1];
-
-                  const sortedStyle = sortedStyles[i];
-                  const sortedNode = sortedStyle.node;
-
                   return [
-                    fixer.replaceTextRange(
-                      [startIndex, endIndex],
-                      context.getSourceCode().getText(sortedNode),
-                    ),
-                    fixer.replaceTextRange(
-                      sortedNode.range,
-                      context.getSourceCode().getText(styles[i].node),
+                    fixer.replaceText(
+                      properties[i],
+                      context.getSourceCode().getText(sortedStyles[i].node),
                     ),
                   ];
                 },
               });
             }
-          }
+          });
         }
       },
     };
   },
 };
+
